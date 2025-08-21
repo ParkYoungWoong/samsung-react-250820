2cab181fc18051f73d5ebbaa94acb954dfde60a1
ParkYoungWoong <thesecon@gmail.com>
2025-08-21 14:52:46 +0900

    08-21 14:52

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import './globals.css'
-import Header from '@/components/Header'
 
 export default function RootLayout({
   children
@@ -8,13 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
-      <body className={`antialiased`}>
-        <Header />
-        {children}
-      </body>
+      <body className={`antialiased`}>{children}</body>
     </html>
   )
 }
-// http://localhost:3000/
-// http://localhost:3000/about
-// http://localhost:3000/signin
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,11 +2,13 @@ import Link from 'next/link'
 
 export default function Header() {
   return (
-    <header>
+    <header className="flex gap-[10px]">
       <Link href="/">Home</Link>
       <Link href="/about">About</Link>
       <Link href="/signin">Sign In</Link>
       <Link href="/movies/tt4520988?plot=full">Sample Movie</Link>
+      <Link href="/async">Async</Link>
+      <Link href="/paralles">Paralles</Link>
     </header>
   )
 }
