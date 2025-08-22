2ab3d2c1f697007ce312a86d4c1a2ba2119e8ada
ParkYoungWoong <thesecon@gmail.com>
2025-08-22 15:22:32 +0900

    08-22 15:22

--- app/(default-layout)/(movies)/movies/[movieId]/page.tsx
+++ app/(default-layout)/(movies)/movies/[movieId]/page.tsx
@@ -1,11 +1,50 @@
 import axios from 'axios'
 import Image from 'next/image'
 import Link from 'next/link'
+import type { Metadata } from 'next'
 
 interface Movie {
   Title: string
   Plot: string
   Poster: string
+}
+
+fetch('')
+
+export const generateMetadata = async ({
+  params,
+  searchParams
+}: {
+  params: Promise<{ movieId: string }>
+  searchParams: Promise<{ plot: string }>
+}): Promise<Metadata> => {
+  const { movieId } = await params
+  const { plot = 'short' } = await searchParams
+  const res = await fetch(
+    `https://omdbapi.com?apikey=7035c60c&i=${movieId}&plot=${plot}`,
+    {
+      cache: 'force-cache'
+    }
+  )
+  const movie: Movie = await res.json()
+
+  const title = movie.Title
+  const description = movie.Plot
+  const image = movie.Poster // 300
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [image]
+    },
+    twitter: {
+      title,
+      description,
+      images: [image]
+    }
+  }
 }
 
 export default async function MovieDetails({
@@ -17,10 +56,13 @@ export default async function MovieDetails({
 }) {
   const { movieId } = await params
   const { plot = 'short' } = await searchParams
-  // await new Promise(resolve => setTimeout(resolve, 2000))
-  const { data: movie } = await axios<Movie>(
-    `https://omdbapi.com?apikey=7035c60c&i=${movieId}&plot=${plot}`
+  const res = await fetch(
+    `https://omdbapi.com?apikey=7035c60c&i=${movieId}&plot=${plot}`,
+    {
+      cache: 'force-cache'
+    }
   )
+  const movie: Movie = await res.json()
 
   // throw new Error('에러가 발생했어유~😘')
 
